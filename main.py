85f64b83b02be5b00f8412d1413380b2d7e8445f
Uday-sidagana <uday.sidgana@gmail.com>
2025-02-16 01:41:22 +0530

    Date picker and Stock titles

--- main.py
+++ main.py
@@ -16,8 +16,17 @@ def load_data(ticker1, ticker2, start, end):
     df2 = yf.download(ticker2, start, end)
     return df1, df2
 
-def plot_data(data, indicators, sync_axix= None):
+def plot_data(data, indicators, sync_axis= None):
     pass
 
 def on_button_click(ticker1, ticker2, start, end, indicators):
     pass
+
+stock1_text = TextInput(title = "Stock 1")
+stock2_text = TextInput(title = "Stock 2")
+
+date_picker_from = DatePicker(title = "Start Date", value ="2020-01-01", min_date='2000-01-01', 
+                              max_date=dt.datetime.now().strftime("%Y-%M-%D"))
+
+date_picker_to = DatePicker(title = "End Date", value ="2020-02-01", min_date='2000-01-01', 
+                            max_date=dt.datetime.now().strftime("%Y-%M-%D"))
